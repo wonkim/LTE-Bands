f6cb383a9486a86733f5e787e2d2f483533e662e
Wonil Kim <wonil.kim@gmail.com>
2012-08-01 00:13:51 +0900

    Remove copyright comments

--- LTE Bands/BandInfoDataController.m
+++ LTE Bands/BandInfoDataController.m
@@ -3,7 +3,6 @@
 //  LTE Bands
 //
 //  Created by Wonil Kim on 2/22/12.
-//  Copyright (c) 2012 Nokia Siemens Networks. All rights reserved.
 //
 
 #import "BandInfoDataController.h"
--- LTE Bands/CarrierInfo.m
+++ LTE Bands/CarrierInfo.m
@@ -3,7 +3,6 @@
 //  LTE Bands
 //
 //  Created by Wonil Kim on 2/28/12.
-//  Copyright (c) 2012 Nokia Siemens Networks. All rights reserved.
 //
 
 #import "CarrierInfo.h"
