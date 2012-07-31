0118dcf3176a8c47cf1d58285e31c3c2677ad32d
Wonil Kim <wonil.kim@gmail.com>
2012-08-01 00:07:54 +0900

    Initial setup

---
+++ LTE Bands/DetailViewController(Wonils-MacBook-Pro.local-2012년 3월 1일 20시 50분 18초).h
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.h
+//  LTE Bands
+//
+//  Created by Wonil Kim on 21/02/12.
+//  Copyright (c) 2012 Nokia Siemens Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BandInfo;
+
+@interface DetailViewController : UITableViewController
+
+@property (strong, nonatomic) BandInfo *band;
+@property (weak, nonatomic) IBOutlet UILabel *upFreqLabel;
+@property (weak, nonatomic) IBOutlet UILabel *downFreqLabel;
+
+@end
